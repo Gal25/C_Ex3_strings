37f3d4d08aec3b4ddaaebcd6413bc2880a563079
Gal25 <92460450+Gal25@users.noreply.github.com>
2021-12-19 15:26:19 +0200

    Add files via upload

---
+++ string.h
@@ -0,0 +1,18 @@
+
+char* place(char ch[], int i , int j);
+
+/////////funcion 1/////////////////////////////
+int culcSUM(char arr[], int len);
+void GematriaSequences(char *text, char *word);
+
+/////////function 2////////////////////////////
+int helpAtbash (char *w, char *t);
+void AtbashSequences(char *text, char *word);
+
+/////////function 3///////////////////////////
+void swap(char *s1 ,char *s2);
+int anagramHelp(char* t,char* w);
+void AnagramSequences(char *text, char *word);
+
+/////////////////main///////////////////
+int main();
